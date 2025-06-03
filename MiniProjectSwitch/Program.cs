895a355300825744e3a94fba95b1ea1f5197f8ae
mgapp <mgapp@MIKEGYO>
2025-06-03 18:14:10 -0400

    Add project files.

---
+++ MiniProjectSwitch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MiniProjectSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name: ");
+            string? firstName = Console.ReadLine();
+            Console.Write("What is your age: ");
+            bool isValidAge = int.TryParse(Console.ReadLine(), out int age);
+            switch (firstName.ToLower())
+            {
+                case "bob" or "sue":
+                    Console.WriteLine($"Hello Professor {firstName}");
+                    break;
+                default:
+                    Console.WriteLine($"Hello {firstName}");
+                    break;
+            }
+            if (isValidAge) {
+                switch (age)
+                {
+                    case < 21:
+                        int X = 21 - age;
+                        Console.WriteLine($"You should wait {X} years to start this class");
+                        break;
+                    default:
+                        Console.WriteLine("Welcome to class.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("You did not provide a valid age.");
+            }
+        }
+    }
+}
